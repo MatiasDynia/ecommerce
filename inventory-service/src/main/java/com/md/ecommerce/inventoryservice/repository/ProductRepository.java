6e917ec05b4103c1309dd463b502fd043259ce71
matias dynia <matiasdynia@gmail.com>
2019-06-05 08:58:54 -0300

    Matias Dynia - ECOM-0013 - improve login and add sleuth

--- inventory-service/src/main/java/com/md/ecommerce/inventoryservice/repository/ProductRepository.java
+++ inventory-service/src/main/java/com/md/ecommerce/inventoryservice/repository/ProductRepository.java
@@ -4,8 +4,10 @@ import com.md.ecommerce.inventoryservice.entity.ProductEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends CrudRepository<ProductEntity, Double> {
 
-    ProductEntity findByCode(String code);
+    Optional<ProductEntity> findByCode(String code);
 }
--- inventory-service/src/main/java/com/md/ecommerce/inventoryservice/service/ProductServiceImpl.java
+++ inventory-service/src/main/java/com/md/ecommerce/inventoryservice/service/ProductServiceImpl.java
@@ -1,11 +1,16 @@
 package com.md.ecommerce.inventoryservice.service;
 
 import com.md.ecommerce.inventoryservice.entity.ProductEntity;
+import com.md.ecommerce.inventoryservice.exception.ProductNotFoundException;
 import com.md.ecommerce.inventoryservice.repository.ProductRepository;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
 
@@ -19,18 +24,36 @@ public class ProductServiceImpl implements ProductService {
     @Override
     @HystrixCommand
     public ProductEntity findByCode(String code) {
-        return productRepository.findByCode(code);
+        log.info("Retrieving product " + code + "...");
+
+        Optional<ProductEntity> productFound = productRepository.findByCode(code);
+
+        if(productFound.isPresent()) {
+            log.info("Product " + code + "found!");
+
+            return productFound.get();
+        } else {
+            throw new ProductNotFoundException("Product " + code + "not found!");
+        }
     }
 
     @Override
     @HystrixCommand
     public Iterable<ProductEntity> findAll() {
+        log.info("Retrieving all products...");
+
         return productRepository.findAll();
     }
 
     @Override
     @HystrixCommand
     public ProductEntity save(ProductEntity product) {
-        return productRepository.save(product);
+        log.info("Saving new product...");
+
+        ProductEntity productCreated = productRepository.save(product);
+
+        log.info("Product " + product.getCode() + " successfully saved!");
+
+        return productCreated;
     }
 }
